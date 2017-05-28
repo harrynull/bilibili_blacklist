6df8d954cdec3e00251c5ae6acd237ee928c5940
Null <abc612008@126.com>
2017-05-28 10:02:50 +0800

    Support add & remove filter

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
+var request = require('request');
 var express = require('express');
 var app = express();
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser');
 
 app.use('/', express.static(__dirname + '/www'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/vue/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/vue-resource/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/login/cookies', function (req, res) {
-   res.cookie("DedeUserID", req.params.DedeUserID);
-   res.cookie("DedeUserID__ckMd5", req.params.DedeUserID__ckMd5);
-   res.cookie("SESSDATA", req.params.SESSDATA);
-   res.redirect("/");
+    res.cookie("bilibili_cookies", "DedeUserID="+req.body["DedeUserID"]+"; DedeUserID__ckMd5="+req.body["DedeUserID__ckMd5"]+"; SESSDATA="+req.body["SESSDATA"]);
+    res.redirect("/");
 })
 
+app.get('/fetch_blacklist', function (req, res) {
+    request(
+        {
+            url: 'https://api.bilibili.com/x/dm/filter/user?jsonp=jsonp',
+            headers: {'Cookie': req.cookies.bilibili_cookies}
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200)
+                res.json(JSON.parse(body)["data"]["rule"]);
+            else
+                res.send("unknown error");
+        }
+    );
+})
+
+app.post('/add_item', function (req, res) {
+    request.post(
+        {
+            url: 'https://api.bilibili.com/x/dm/filter/user/add',
+            headers: {'Cookie': req.cookies.bilibili_cookies},
+            form: {"type":req.body["type"],"filter":req.body["filter"],"jsonp":"jsonp","csrf":""}
+            //type:0/1/2 filter:content jsonp:jsonp csrf:
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200){
+                res.json(JSON.parse(body));
+            }else{
+                res.send("unknown error");
+            }
+        }
+    );
+})
+app.post('/del_item', function (req, res) {
+    request.post(
+        {
+            url: 'https://api.bilibili.com/x/dm/filter/user/del',
+            headers: {'Cookie': req.cookies.bilibili_cookies},
+            form: {"ids":req.body["ids"],"jsonp":"jsonp","csrf":""}
+            //ids:889648 jsonp:jsonp csrf:
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200)
+                res.json(JSON.parse(body));
+            else
+                res.send("unknown error");
+        }
+    );
+})
 var server = app.listen(8000)
